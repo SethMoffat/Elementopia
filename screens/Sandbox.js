354559142637926114ed482d4d4afd2dccbbaa11
Seth Moffat <sethmoffat1@gmail.com>
2023-04-27 19:51:28 -0600

    gave up

---
+++ screens/Sandbox.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+
+export default function Sandbox() {
+  const [grid, setGrid] = useState(/*... initialize empty grid */);
+
+  const handleDrag = (element) => {
+    //... handle element drag
+  };
+
+  const handleDrop = (element, target) => {
+    //... handle element drop and combination
+  };
+
+  return (
+    <View>
+      {/*... render grid, elements, and interactions */}
+    </View>
+  );
+}
